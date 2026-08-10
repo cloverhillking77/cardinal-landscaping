77503d549761be761c6e9be513528ecfe26510fb
cloverhillking77 <rockytopdevshop@gmail.com>
2026-08-10 10:08:21 -0400

    Install new Cardinal Google Ads tag site-wide

--- site.js
+++ site.js
@@ -1,3 +1,25 @@
+const CARDINAL_GOOGLE_ADS_ID = 'AW-18376798236';
+
+function initCardinalGoogleAdsTag() {
+  if (window.__cardinalGoogleAdsInitialized) return;
+  window.__cardinalGoogleAdsInitialized = true;
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function gtag(){ window.dataLayer.push(arguments); };
+  window.gtag('js', new Date());
+  window.gtag('config', CARDINAL_GOOGLE_ADS_ID);
+
+  const hasGoogleTagLibrary = document.querySelector('script[src*="googletagmanager.com/gtag/js"]');
+  if (!hasGoogleTagLibrary) {
+    const tag = document.createElement('script');
+    tag.async = true;
+    tag.src = `https://www.googletagmanager.com/gtag/js?id=${CARDINAL_GOOGLE_ADS_ID}`;
+    document.head.appendChild(tag);
+  }
+}
+
+initCardinalGoogleAdsTag();
+
 const menuButton = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.nav-links');
 const serviceMenu = document.querySelector('.service-menu');
